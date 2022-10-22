f6b9505a2376901884eba0f1dfcb0dbcd291dd6b
Ricard <ricardpons@me.com>
2022-10-22 21:14:15 +0200

    add navigation placeholder items

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Filament\Facades\Filament;
+use Filament\Navigation\NavigationItem;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +25,26 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Filament::serving(function () {
+            Filament::registerNavigationItems([
+                NavigationItem::make('Profile')
+                    ->url(route('filament.pages.my-profile'), shouldOpenInNewTab: false)
+                    ->isActiveWhen(fn (): bool => request()->routeIs('filament.pages.my-profile'))
+                    ->icon('heroicon-o-user')
+                    ->group('Account')
+                    ->sort(1),
+                NavigationItem::make('Subscription')
+                    ->url('https://filament.pirsch.io', shouldOpenInNewTab: true)
+                    ->icon('heroicon-o-credit-card')
+                    ->group('Account')
+                    ->sort(2),
+
+                NavigationItem::make('Support & Feedback')
+                    ->url('https://filament.pirsch.io', shouldOpenInNewTab: true)
+                    ->icon('heroicon-o-support')
+                    ->group('Support')
+                    ->sort(1),
+            ]);
+        });
     }
 }
