1d7165f54dce84312119c2ea2ad154b929ad42af
daniel_jao <daniel_jao@yahoo.com>
2023-01-30 19:14:45 +0800

    fixes for the score

--- resources/views/filament/resources/newsletter-resource/partials/scanningOrFinished.blade.php
+++ resources/views/filament/resources/newsletter-resource/partials/scanningOrFinished.blade.php
@@ -125,7 +125,8 @@
 		</div>
 	</div>
   <?php }?>
-<script src="//code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
+  
+<!--   <script src="//code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script> -->
   <script type="text/javascript">
 function toggleReport() {
 	$('.report-section').toggle();
