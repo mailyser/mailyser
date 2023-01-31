fb052f557093ae11f4046e1c9bae9f71431daddd
daniel_jao <daniel_jao@yahoo.com>
2023-02-01 01:13:36 +0800

    debugging

--- app/Filament/Pages/SendingAddress.php
+++ app/Filament/Pages/SendingAddress.php
@@ -34,9 +34,6 @@ class SendingAddress extends Page implements HasForms
 
     protected static function shouldRegisterNavigation(): bool
     {
-        var_dump(auth()->user()->senders->count());
-        die;
-        
         return ! (bool) auth()->user()->senders->count();
     }
 
