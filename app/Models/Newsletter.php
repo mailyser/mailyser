7faf5387a280093f591a7daea084cdaa38e8d00c
daniel_jao <daniel_jao@yahoo.com>
2023-04-13 20:03:36 +0800

    debugging

--- app/Models/Newsletter.php
+++ app/Models/Newsletter.php
@@ -178,6 +178,9 @@ class Newsletter extends Model
         // Will dump a beauty json :3
         $mailTestJson = (json_decode($result, true));
         
+        var_dump($mailTestJson);
+        die;
+        
         return $mailTestJson;
       
     }
